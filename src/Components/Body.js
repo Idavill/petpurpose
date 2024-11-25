aad1b5c3bee74062466475ad726fdef2b3b64086
Idavill <idamariavilladsen@gmail.com>
2024-11-25 10:10:06 +0100

    Snap works and freezes camera, + removed predic from loop, and removed stats

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -5,9 +5,13 @@ import AnimalPurpose from "../Data/AnimalPurposes.json";
 
 export default function Body() {
   const URL = "https://teachablemachine.withgoogle.com/models/K31bjxpYW/";
-  let model, webcam, labelContainer, maxPredictions;
-  let requestId;
+  //let model, webcam, labelContainer, maxPredictions;
   const stringExample = "stringexample";
+  const [webcamReady, setWebcamReady] = useState(false);
+  const [model, setModel] = useState(false);
+  const [labelContainer, setLabelContainer] = useState(false);
+  const [maxPredictions, setMaxPredictions] = useState(false);
+  const [webCam, setWebcam] = useState(null);
   const [initCalled, setInitCalled] = useState(false);
   const [randomPick, setRandomPick] = useState("");
   const [className, setClassName] = useState("");
@@ -16,10 +20,6 @@ export default function Body() {
     attribute: "",
     manifest: "",
   });
-
-  useEffect(() => {
-    console.log("WEBBBBBBBBBB changed", webcam);
-  }, [webcam]);
 
   useEffect(() => {
     if (className != "") {
@@ -39,121 +39,108 @@ export default function Body() {
   }, [className]);
 
   async function init() {
-    setInitCalled(true);
-    const modelURL = URL + "model.json";
-    const metadataURL = URL + "metadata.json";
+    if (!webCam) {
+      // checking on react variable since let doesn't hold the info
+      setInitCalled(true);
+      const modelURL = URL + "model.json";
+      const metadataURL = URL + "metadata.json";
 
-    // load the model and metadata
-    // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
-    // or files from your local hard drive
-    // Note: the pose library adds "tmImage" object to your window (window.tmImage)
-    model = await tmImage.load(modelURL, metadataURL);
-    maxPredictions = model.getTotalClasses();
+      // load the model and metadata
+      // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
+      // or files from your local hard drive
+      // Note: the pose library adds "tmImage" object to your window (window.tmImage)
+      let m = await tmImage.load(modelURL, metadataURL);
+      let mp = m.getTotalClasses();
 
-    // Convenience function to setup a webcam
-    const flip = true; // whether to flip the webcam
-    webcam = new tmImage.Webcam(200, 200, flip); // width, height, flip
-    await webcam.setup(); // request access to the webcam
-    await webcam.play();
-    window.requestAnimationFrame(loop);
+      setMaxPredictions(mp);
+      setModel(m);
 
-    // append elements to the DOM
-    document.getElementById("webcam-container").appendChild(webcam.canvas);
-    labelContainer = document.getElementById("label-container");
+      const flip = true; // whether to flip the webcam
+      const newWebcam = new tmImage.Webcam(200, 200, flip); // width, height, flip
+      setWebcam(newWebcam);
+    }
+  }
+
+  const appendWebCamElements = async (wc) => {
+    await wc.setup(); // request access to the webcam
+    await wc.play();
+
+    console.log("wc = ", wc);
+    document.getElementById("webcam-container").appendChild(wc.canvas);
+    let lc = document.getElementById("label-container");
     for (let i = 0; i < maxPredictions; i++) {
       // and class labels
-      labelContainer.appendChild(document.createElement("div"));
+      lc.appendChild(document.createElement("div"));
     }
-  }
 
-  // async function init() {
-  //   setInitCalled(true);
-  //   const modelURL = URL + "model.json";
-  //   const metadataURL = URL + "metadata.json";
-  //   model = await tmImage.load(modelURL, metadataURL);
-  //   maxPredictions = model.getTotalClasses();
+    setLabelContainer(lc);
+    setWebcamReady(true);
+    window.requestAnimationFrame(loop);
+  };
 
-  //   const flip = true; // whether to flip the webcam
-  //   webcam = new tmImage.Webcam(200, 200, flip); // width, height, flip
-  //   await webcam.setup(); // request access to the webcam
-  //   await webcam.play();
-  //   window.requestAnimationFrame(loop);
-  //   requestId = window.requestAnimationFrame(loop);
-
-  //   document.getElementById("webcam-container").appendChild(webcam.canvas);
-  //   labelContainer = document.getElementById("label-container");
-  //   for (let i = 0; i < maxPredictions; i++) {
-  //     labelContainer.appendChild(document.createElement("div"));
-  //   }
-  // }
+  useEffect(() => {
+    if ((webCam, maxPredictions)) {
+      appendWebCamElements(webCam);
+    }
+  }, [webCam, maxPredictions]);
 
   async function loop() {
-    webcam.update(); // update the webcam frame
-    console.log(webcam, "is updating <3 ");
-    await predict();
+    if (webCam) {
+      webCam.update(); // update the webcam frame
+    }
+    //await predict(webCam);
     window.requestAnimationFrame(loop);
   }
 
-  // async function predict() {
-  //   const prediction = await model.predict(webcam.canvas);
-  //   for (let i = 0; i < maxPredictions; i++) {
-  //     const classPrediction =
-  //       prediction[i].className + ": " + prediction[i].probability.toFixed(2);
-  //     labelContainer.childNodes[i].innerHTML = classPrediction;
-  //   }
-
-  //   return prediction;
-  // }
-
-  async function predict() {
+  async function predict(wc) {
     // predict can take in an image, video or canvas html element
-    const prediction = await model.predict(webcam.canvas);
-    for (let i = 0; i < maxPredictions; i++) {
-      const classPrediction =
-        prediction[i].className + ": " + prediction[i].probability.toFixed(2);
-      labelContainer.childNodes[i].innerHTML = classPrediction;
-    }
+    console.log("webCam = ", wc);
+
+    const prediction = await model.predict(wc.canvas);
+    // for (let i = 0; i < maxPredictions; i++) {
+    //   const classPrediction =
+    //     prediction[i].className + ": " + prediction[i].probability.toFixed(2);
+    //   labelContainer.childNodes[i].innerHTML = classPrediction;
+    // }
 
     return prediction;
   }
 
   async function snap() {
-    try {
-      if (webcam) {
-        webcam.stop();
-        const predics = await predict();
+    if (webCam) {
+      //console.log("webcam = ", webcam);
+      console.log("webCam = ", webCam);
+      // webCam.update();
+      const predics = await predict(webCam);
+      webCam.stop();
 
-        let maxProb = -1;
-        let maxClass = "";
-        predics.forEach((element, i) => {
-          const currentProb = element.probability.toLocaleString("fullwide", {
-            useGrouping: false,
-          });
-          if (i - 1 >= 0) {
-            if (currentProb > maxProb) {
-              maxProb = currentProb; // Update max probability
-              maxClass = element.className;
-            }
-            console.log(element);
-          } else {
+      let maxProb = -1;
+      let maxClass = "";
+      predics.forEach((element, i) => {
+        const currentProb = element.probability.toLocaleString("fullwide", {
+          useGrouping: false,
+        });
+        if (i - 1 >= 0) {
+          if (currentProb > maxProb) {
             maxProb = currentProb; // Update max probability
             maxClass = element.className;
           }
-        });
-        console.log(maxClass, " ", maxProb);
+          console.log(element);
+        } else {
+          maxProb = currentProb; // Update max probability
+          maxClass = element.className;
+        }
+      });
+      console.log(maxClass, " ", maxProb);
 
-        setClassName(maxClass);
-      } else {
-        console.log("no webbiedebbie");
-      }
-    } catch (e) {
-      console.error(e);
+      setClassName(maxClass);
+    } else {
+      console.log("no webbiedebbie");
     }
   }
 
   return (
     <div>
-      {" "}
       <button className="startButton" type="button" onClick={() => init()}>
         FIND YOUR DESTINY
       </button>
@@ -169,8 +156,8 @@ export default function Body() {
       >
         FIND YOUR DESTINY
       </button> */}
-      {initCalled ? (
-        <button type="button" onClick={() => snap()}>
+      {webCam ? (
+        <button type="button" onClick={() => snap()} disabled={!webcamReady}>
           SNAP
         </button>
       ) : null}
